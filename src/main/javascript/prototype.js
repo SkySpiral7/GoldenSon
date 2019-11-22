1acc98fcd11f3c50ec144e506132e3d6d667e206
Ryan Worcester <rworcest@g.emporia.edu>
2019-11-22 16:37:15 -0600

    prototype.js no longer polutes

--- src/main/javascript/prototype.js
+++ src/main/javascript/prototype.js
@@ -1,33 +1,50 @@
 'use strict';
 if (undefined === JSON.clone)
 {
-   JSON.clone = function (obj)
-   {
-      return JSON.parse(JSON.stringify(obj));
-   };
+   Object.defineProperty(JSON, 'clone', {
+      value: function (obj)
+      {
+         return JSON.parse(JSON.stringify(obj));
+      }
+   })
 }
-/**For each element of this array a type strict comparison is done against the parameter and returns true if any of them match*/
-if (Array.prototype.contains === undefined)
+if (undefined === Array.prototype.contains)
 {
-   Array.prototype.contains = function (obj)
-   {
-      return (this.indexOf(obj) !== -1);  //Array.prototype.indexOf is type strict (using ===)
-   };
+   Object.defineProperty(Array.prototype, 'contains', {
+      value:
+         /**For each element of this array a type strict comparison is done against the parameter and returns true if any of them match*/
+         function (obj)
+         {
+            return (this.indexOf(obj) !== -1);  //Array.prototype.indexOf is type strict (using ===)
+         }
+   });
 }
-/**Removes the element from this array that is located at the index specified.
- Negative index is counted from the end.
- If the index does not exist nothing happens.
- This array is changed and nothing is returned.*/
-Array.prototype.remove = function (index)
+if (undefined === Array.prototype.remove)
 {
-   if (typeof(index) === 'number' && isFinite(index)) this.splice(index, 1);
-};
-/**Removes the first element from this array that is equal to (type strict) the value specified.
- If the value is not found, nothing happens.
- This array is changed and nothing is returned.*/
-Array.prototype.removeByValue = function (value)
+   Object.defineProperty(Array.prototype, 'remove', {
+      value:
+         /**Removes the element from this array that is located at the index specified.
+          Negative index is counted from the end.
+          If the index does not exist nothing happens.
+          This array is changed and nothing is returned.*/
+         function (index)
+         {
+            if (typeof(index) === 'number' && isFinite(index)) this.splice(index, 1);
+         }
+   });
+}
+if (undefined === Array.prototype.removeByValue)
 {
-   var index = this.indexOf(value);
-   if (-1 === index) return;  //not found
-   this.remove(index);
-};
+   Object.defineProperty(Array.prototype, 'removeByValue', {
+      value:
+         /**Removes the first element from this array that is equal to (type strict) the value specified.
+          If the value is not found, nothing happens.
+          This array is changed and nothing is returned.*/
+         function (value)
+         {
+            var index = this.indexOf(value);
+            if (-1 === index) return;  //not found
+            this.remove(index);
+         }
+   });
+}
